afd6ee72a33feb11e80842600b2eab4fefa7e31a
jonjon98 <74106692+jonjon98@users.noreply.github.com>
2021-06-16 22:00:41 +0800

    no message

--- screens/Consult.js
+++ screens/Consult.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Styled from 'styled-components/native';
-import MapView, {Marker} from 'react-native-maps';
+import React from "react";
+import Styled from "styled-components/native";
+import MapView, { Marker } from "react-native-maps";
 
 const Container = Styled.View`
     flex: 1;
@@ -10,14 +10,15 @@ const MarkerOnMap = () => {
   return (
     <Container>
       <MapView
-        style={{flex: 1}}
+        style={{ flex: 1 }}
         initialRegion={{
           latitude: 1.33018,
           longitude: 103.92665,
-          title="My House"
-        }}>
+          title: "My House",
+        }}
+      >
         <Marker
-          coordinate={{latitude: 37.78825, longitude: 103.92588}}
+          coordinate={{ latitude: 37.78825, longitude: 103.92588 }}
           //image{{uri:'custom_pin_clinic 1'}}
           title="YSL Bedok Clinic & Surgery"
           description="Wednesday
@@ -50,8 +51,7 @@ const MarkerOnMap = () => {
           8am–12:30pm
           2–5pm
           6:30–9:30pm"
-
-          coordinate={{latitude: 1.33305, longitude: 193.92603}}
+          coordinate={{ latitude: 1.33305, longitude: 193.92603 }}
           //image{{uri:'custom_pin_clinic 2'}}
           title="S.T. Medical Group"
           description="Wednesday
@@ -82,7 +82,6 @@ const MarkerOnMap = () => {
           8:30am–1pm
           7–9pm"
         />
-        
       </MapView>
     </Container>
   );
